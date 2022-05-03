1615617d9a2b2f6bb249d38313c085c232e29a08
Elly run <ellyrun@gmail.com>
2022-05-03 15:43:34 +0000

    byrja á að geta create-að item
    Ellý og Kristín /python

--- FireSale/create/views.py
+++ FireSale/create/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from create.forms.item_form import ItemCreateForm
+
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'create/index.html')
+
+
+def create_item(request):
+    if request.method == 'POST':
+        print(1)
+    else:
+        form = ItemCreateForm()
+    return render(request, 'create/create_item.html', {
+    })
