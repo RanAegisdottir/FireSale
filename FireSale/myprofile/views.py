36d90a9c91c38b8f20fd50e5b326b0a0cfb654fd
MiniNinjaSwag <ran@snillingar.com>
2022-05-03 16:05:52 +0000

    láta data birta í html
    allt data birtist í shop

--- FireSale/myprofile/views.py
+++ FireSale/myprofile/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from myprofile.models import Users
 
 # Create your views here.
 def index(request):
-    return render(request, 'myprofile/index.html')
+    return render(request, 'myprofile/index.html', {'Users': Users.objects.all()})
--- FireSale/shop/views.py
+++ FireSale/shop/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-
-products = [
-    {'name': 'shoes', 'price': 30},
-    {'name': 'car', 'price': 200}
-]
+from django.shortcuts import render, get_object_or_404
+from shop.models import Item
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html', context={ 'products': products })
+    context = {'products': Item.objects.all().order_by('name')}
+    return render(request, 'shop/index.html', context)
+
+def get_item_by_id(request, id):
+    return render(request, 'shop/item_details.html', {
+        'Item': get_object_or_404(Item, pk=id)
+    })
