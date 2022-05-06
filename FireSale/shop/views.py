5e35dc691701bb14fd0614116271b82d4b699d0b
Elly run <ellyrun@gmail.com>
2022-05-06 13:21:51 +0000

    profile mynd komin i nav barinn

--- FireSale/shop/views.py
+++ FireSale/shop/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+
+from myprofile.models import UserImage
 from shop.models import Item
 
 # Create your views here.
 def index(request):
-    context = {'products': Item.objects.all().order_by('name')}
+    context = {'products': Item.objects.all().order_by('name'),
+               'Image': UserImage.objects.get(user_id=request.user.id)}
+
     return render(request, 'shop/index.html', context)
 
 def get_item_by_id(request, id):
     return render(request, 'shop/item_details.html', {
-        'Item': get_object_or_404(Item, pk=id)
+        'Item': get_object_or_404(Item, pk=id),
+        'Image': UserImage.objects.get(user_id=request.user.id)
     })
