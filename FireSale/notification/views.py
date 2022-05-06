a89101e719dd2cbc7f2721ccfe4e5d558aae6a43
Elly run <ellyrun@gmail.com>
2022-05-06 12:03:14 +0000

    vinna eh i notification

--- FireSale/notification/views.py
+++ FireSale/notification/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from notification.models import Notifications
+from shop.models import Offers
 
 
 def index(request):
-    return render(request, 'notification/notifications.html')
+    return render(request, 'notification/notifications.html',
+                  {'User': request.user,
+                   'Notification': Notifications.objects.all()})
