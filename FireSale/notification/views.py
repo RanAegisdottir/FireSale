95bf0070ed12363ffad4b20cbf9055f5123396d2
Elly run <ellyrun@gmail.com>
2022-05-08 12:46:59 +0000

    profile mynd í notification

--- FireSale/notification/views.py
+++ FireSale/notification/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+
+from myprofile.models import UserImage
 from notification.models import Notifications
 from shop.models import Offers
 
@@ -6,4 +8,5 @@ from shop.models import Offers
 def index(request):
     return render(request, 'notification/notifications.html',
                   {'User': request.user,
-                   'Notification': Notifications.objects.all()})
+                   'Notification': Notifications.objects.all(),
+                   'Image': UserImage.objects.get(user_id=request.user.id)})
