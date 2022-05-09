46cb3f97a7e18f2de3d889e4564251bc6a78869e
Elly run <ellyrun@gmail.com>
2022-05-09 08:53:10 +0000

    meira í checkout

--- FireSale/checkout/forms/checkout_form.py
+++ FireSale/checkout/forms/checkout_form.py
@@ -11,9 +11,9 @@ class CheckoutForm(ModelForm):
             'city': widgets.TextInput(attrs={'class': 'form-control'}),
             'phone': widgets.NumberInput(attrs={'class': 'form-control'}),
             'country': widgets.Select(attrs={'class': 'form-control'}),
-            'company name': widgets.TextInput(attrs={'class': 'form-control'}),
-            'card name': widgets.TextInput(attrs={'class': 'form-control'}),
-            'card number': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'company_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'card_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'card_number': widgets.NumberInput(attrs={'class': 'form-control'}),
             'exdate': widgets.TextInput(attrs={'class': 'form-control'}),
             'CVC': widgets.NumberInput(attrs={'class': 'form-control'})
         }
