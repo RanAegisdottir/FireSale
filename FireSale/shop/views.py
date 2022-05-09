5b1dc669da248a65eee31c45179f6a106a8cb84b
MiniNinjaSwag <ran@snillingar.com>
2022-05-09 11:09:22 +0000

    search bar og similar items virka

--- FireSale/shop/views.py
+++ FireSale/shop/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from myprofile.models import UserImage
@@ -5,14 +6,14 @@ from shop.models import Item
 
 # Create your views here.
 def index(request):
-    if 'search-filter' in request.GET:
-        search_filter = request.GET['search-filter']
+    if 'search_filter' in request.GET:
+        search_filter = request.GET['search_filter']
         products = [{
             'id': x.id,
             'name': x.name,
             'priceidea': x.priceidea,
             'image': x.itemimage_set.first().imgURL
-        } for x in Item.objects.filter(name_icontains=search_filter)]
+        } for x in Item.objects.filter(name__icontains=search_filter)]
         return JsonResponse({'data': products})
 
     context = {'products': Item.objects.all().order_by('name'),
@@ -20,7 +21,9 @@ def index(request):
     return render(request, 'shop/index.html', context)
 
 def get_item_by_id(request, id):
+    products = Item.objects.filter(~Q(pk=id))[:3]
     return render(request, 'shop/item_details.html', {
+        'products': products,
         'Item': get_object_or_404(Item, pk=id),
         'Image': UserImage.objects.get(user_id=request.user.id)
     })
